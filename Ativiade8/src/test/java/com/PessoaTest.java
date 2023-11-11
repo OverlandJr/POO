43e3a1b41269327f11cded67c28b6e6a84681a6a
OverlandJr <127855317+OverlandJr@users.noreply.github.com>
2023-11-11 01:18:34 -0300

    Add files via upload

---
+++ Ativiade8/src/test/java/com/PessoaTest.java
@@ -0,0 +1,23 @@
+package com.exemplo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PessoaTest {
+    Pessoa pessoa;
+
+    @BeforeEach
+    public void setUp(){
+        this.pessoa = new Pessoa();
+    }
+
+    @Test
+    public void dizerOlaTest(){
+        assertEquals("Olá, Maven!", pessoa.dizerOla());
+    }
+
+    
+
+
+}
